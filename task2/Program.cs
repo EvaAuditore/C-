67ced17331dc529411b8b113e490af8146c291e5
Eva_Auditore <Murzina.T.N@yandex.ru>
2022-09-01 01:03:10 +0700

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+string x = (Console.ReadLine());
+
+if (x == "1")
+  {
+    Console.WriteLine("Понедельник");
+  }
+else if (x == "2")
+  {
+    Console.WriteLine("Вторник");
+  }
+  else if (x == "3")
+  {
+    Console.WriteLine("Среда");
+  }
+  else if (x == "4")
+  {
+    Console.WriteLine("четверг");
+  }
+  else if (x == "5")
+  {
+    Console.WriteLine("Пятница");
+  }
+  else if (x == "6")
+  {
+    Console.WriteLine("Суббота");
+  }
+  else if (x == "7")
+  {
+    Console.WriteLine("Воскресене");
+  }
+  else
+  {
+    Console.WriteLine("таких не бывает");
+  }
