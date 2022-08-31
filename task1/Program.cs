4f30bc853f9b9e02a9ecaf57dfce11441b07dec4
Eva_Auditore <Murzina.T.N@yandex.ru>
2022-09-01 00:07:28 +0700

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number2 == number1*number1)
+  {
+    Console.WriteLine($"{number2} является квадратом {number1}");
+  }
+else
+  {
+    Console.WriteLine($"{number2} НЕ является квадратом {number1}");
+  }
