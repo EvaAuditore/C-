c7629fc435b390506996256b819e8590bc04e6be
Eva_Auditore <Murzina.T.N@yandex.ru>
2022-09-01 14:06:41 +0700

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = number * (-1);
+
+while (count <= number)
+  {
+    Console.Write(count + ",");
+    count++;
+  }
+
