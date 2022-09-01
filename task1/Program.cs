540bc0b93d06518cee3c79610aa03b4c189cc211
Eva_Auditore <Murzina.T.N@yandex.ru>
2022-09-01 14:06:31 +0700

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("введите число 1: ");
-int number1 = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine());
 Console.WriteLine("введите число 2: ");
 int number2 = int.Parse(Console.ReadLine());
 if (number2 == number1*number1)
