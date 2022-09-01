986870c4e9f07a180e1430df2ac4b8f8bc697e30
Eva_Auditore <Murzina.T.N@yandex.ru>
2022-09-01 14:06:54 +0700

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿//вводим трехзначное число, на выходе третья цифра числа
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number >=100 && number < 1000)
+{
+    Console.WriteLine(number % 10);
+}
+else 
+{
+    Console.WriteLine("Введите верное значение");
+}
