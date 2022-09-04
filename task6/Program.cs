b3ce51572e4753528ba3c6042afed555fdb1635e
Eva_Auditore <Murzina.T.N@yandex.ru>
2022-09-04 15:04:59 +0700

    добавлены задания с практики и дз

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая выводит случайное
+//трёхзначное число и удаляет вторую цифру этого
+//числа.
+
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a2 = num % 10;
+Console.WriteLine(num);
+Console.WriteLine($"Итоговое число {a1}{a2}");
+
---
+++ task7/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая будет принимать на
+//вход два числа и выводить, является ли второе число
+//кратным первому. Если число 2 не кратно числу 1, то
+//программа выводит остаток от деление.
+//34, 5 -> не кратно, остаток 4
+//16, 4 -> кратно
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number1 % number2 ==0)
+{
+    Console.WriteLine($"{number1},{number2} -> кратно");
+}
+else
+{
+    int number3 = number1 % number2;
+    Console.WriteLine($"{number1},{number2} -> не кратно, остаток {number3}");
+}
