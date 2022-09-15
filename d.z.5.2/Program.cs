a4e1081404090463cc3eeae3974f8c8008376815
Eva_Auditore <Murzina.T.N@yandex.ru>
2022-09-15 18:48:12 +0700

    добавлено ДЗ к 5 семинару

---
+++ d.z.5.2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+int[] array = GetArray(4, 0, 98);
+
+int Method(int[] array)
+{
+    // int count = 0;
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+           sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+int sum = Method(array);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sum}");
---
+++ d.z.5.3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+double[] array = GetArray(5, 0, 98);
+
+double Method(double[] array)
+{
+    double MaxNumber = array[0];
+    double MinNumber = array[0];
+    double difference = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (MaxNumber < array[i])
+        {
+            MaxNumber = array[i];
+        }
+        if (MinNumber > array[i])
+        {
+            MinNumber = array[i];
+        }
+    }
+    difference = MaxNumber - MinNumber;
+    return difference;
+}
+double difference = Method(array);
+Console.WriteLine($"Разница между между максимальным и минимальным элементами: {difference}");
