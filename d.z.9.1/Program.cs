6f5ce4e9898565cb7a053453b97b86f532ce4eea
Eva_Auditore <Murzina.T.N@yandex.ru>
2022-10-03 02:07:44 +0700

    добавлено ДЗ к 9 семинару

---
+++ d.z.9.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2"
+
+
+Console.Clear();
+Console.WriteLine("Введите число N");
+int a = int.Parse(Console.ReadLine());
+
+string Numbers(int n)
+{
+//if (n == 1 && n%10 == 0)
+if (n == 2)
+{
+return "2";
+}
+else
+return n + Numbers(n - 1);
+}
+Console.WriteLine(Numbers(a));
---
+++ d.z.9.2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+//  натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.WriteLine("Введите число M");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма ({a},{b}) = {SumMN(a - 1, b)}");
+
+int SumMN(int a, int b)
+{
+    int result = a;
+    if (a == b)
+        return 0;
+    else
+    {
+        a++;
+        result = a + SumMN(a, b);
+        return result;
+    }
+}
